2bd216e711115b76d6756216edd2a6463b6dc51c
AGSTRANGER <hannibaletunisien@gmail.com>
2023-10-28 13:02:33 +0100

    Changed how image is made responsive

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,46 +1,46 @@
-import React from "react";
-import { Player } from "../types";
+  import React from "react";
+  import { Player } from "../types";
 
-interface PlayerProps {
-  player: Player;
-}
+  interface PlayerProps {
+    player: Player;
+  }
 
-const PlayerComponent: React.FC<PlayerProps> = ({ player }) => {
-  const {
-    firstname,
-    lastname,
-    shortname,
-    sex,
-    picture: { url: pictureUrl },
-    country: { code },
-    stats: { rank, age },
-  } = player;
+  const PlayerComponent: React.FC<PlayerProps> = ({ player }) => {
+    const {
+      firstname,
+      lastname,
+      shortname,
+      sex,
+      picture: { url: pictureUrl },
+      country: { code },
+      stats: { rank, age },
+    } = player;
 
-  return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
-<img src={pictureUrl} alt={`${firstname} ${lastname}`} className="sm:w-64 sm:h-64 md:w-96 md:h-96 lg:w-128 lg:h-128" />
-        <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">
-          {firstname} {lastname}
+    return (
+      <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
+  <img src={pictureUrl} alt={`${firstname} ${lastname}`} className="w-full h-auto" />
+          <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">
+            {firstname} {lastname}
+          </div>
+          <p className="text-gray-700 text-base">
+            Short Name: {shortname}
+          </p>
+          <p className="text-gray-700 text-base">
+            Sex: {sex}
+          </p>
+          <p className="text-gray-700 text-base">
+            Country Code: {code}
+          </p>
+          <p className="text-gray-700 text-base">
+            Rank: {rank}
+          </p>
+          <p className="text-gray-700 text-base">
+            Age: {age}
+          </p>
         </div>
-        <p className="text-gray-700 text-base">
-          Short Name: {shortname}
-        </p>
-        <p className="text-gray-700 text-base">
-          Sex: {sex}
-        </p>
-        <p className="text-gray-700 text-base">
-          Country Code: {code}
-        </p>
-        <p className="text-gray-700 text-base">
-          Rank: {rank}
-        </p>
-        <p className="text-gray-700 text-base">
-          Age: {age}
-        </p>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default PlayerComponent;
+  export default PlayerComponent;
