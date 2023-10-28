8f27debf50b7702b4543cf49378be52881f65802
AGSTRANGER <hannibaletunisien@gmail.com>
2023-10-28 12:23:33 +0100

    Configured tailwind css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'tailwindcss/base.css';
+import 'tailwindcss/components.css';
+import 'tailwindcss/utilities.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
