d4b86fa47e1be0d68248c6e559e42ca1e8cb9173
AGSTRANGER <hannibaletunisien@gmail.com>
2023-10-29 14:52:01 +0100

    Small improvement to match styling

--- src/components/Matches/Match.tsx
+++ src/components/Matches/Match.tsx
@@ -11,9 +11,13 @@ const MatchComponent: React.FC<MatchProps> = ({ match }) => {
   return (
     <div className="bg-yellow-100 p-4 rounded shadow-md mb-4">
       <h4 className="text-indigo-600 text-lg font-semibold mb-2">Match ID: {match.id}</h4>
-      <p className="text-indigo-600">Start Time: {formatDate(match.startTime) }</p>
-      <p className="text-indigo-600">End Time: {formatDate(match.endTime)}</p>
-      <h5 className="text-red-600 text-lg font-semibold mt-4 mb-2">Players:</h5>
+      <p className="text-gray-700 text-base">
+          <span className="font-bold text-teal-600">Start Time:</span> {formatDate(match.startTime) }
+        </p>
+        <p className="text-gray-700 text-base">
+          <span className="font-bold text-teal-600">End Time:</span> {formatDate(match.endTime) }
+        </p>
+      <h5 className="text-indigo-600 text-lg font-semibold mt-4 mb-2">Players:</h5>
       <ul className="list-disc ml-6">
       {match.players.map(player=>(
   <li key={player.id} className="text-gray-600">
