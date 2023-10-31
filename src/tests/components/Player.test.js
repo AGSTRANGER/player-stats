11d9e4ebab28ed6cdd69b05c8d138b05cae949f6
AGSTRANGER <hannibaletunisien@gmail.com>
2023-10-31 14:21:00 +0100

    Implemented Player component test

--- src/tests/components/Player.test.js
+++ src/tests/components/Player.test.js
@@ -1,32 +1,25 @@
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import PlayerComponent from "../../components/Players/Player";
-// Couldn't install this module
 import configureStore from "redux-mock-store";
+import { players, matches } from "../sampleData";
 
-// Mock your Redux store
 const mockStore = configureStore([]);
 const store = mockStore({
   matches: {
-    matches: [],
+    matches: matches,
   },
 });
 
 describe("Player", () => {
   it("should render player name", () => {
-    const playerData = {
-      id: 1,
-      firstname: "John",
-      lastname: "Doe",
-    };
-
     const { getByText } = render(
       <Provider store={store}>
-        <PlayerComponent player={playerData} onClick={() => {}} />
+        <PlayerComponent player={players[0]} onClick={() => {}} />
       </Provider>
     );
 
-    const playerName = `${playerData.firstname} ${playerData.lastname}`;
+    const playerName = `${players[0].firstname} ${players[0].lastname}`;
     expect(getByText(playerName)).toBeInTheDocument();
   });
 });
--- src/tests/components/TestComponent.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import TestComponent from "../../components/TestComponent";
-
-test("On initial render, TestComponent should be displayed", () => {
-  render(<TestComponent />);
-  expect(screen.getByRole("button", { name: /pay/i })).toBeDisabled();
-  screen.debug();
-});
