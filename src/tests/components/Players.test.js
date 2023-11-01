98e8e74f4758527213ae09e60adc6e24e59d5ae2
AGSTRANGER <hannibaletunisien@gmail.com>
2023-11-01 08:24:34 +0100

    Made players component more detailed

--- src/tests/components/Players.test.js
+++ src/tests/components/Players.test.js
@@ -7,6 +7,10 @@ import { players, matches } from "../sampleData";
 import Players from "../../components/Players/Players";
 import { LOAD_PLAYERS } from "../../GraphQL/PlayerQueries";
 import { LOAD_MATCHES } from "../../GraphQL/MatchQueries";
+import {
+  calculateTotalPlayTime,
+  getWinsAndLossesByPlayer,
+} from "../../helpers/playerHelpers";
 
 const mockStore = configureStore([]);
 const store = mockStore({
@@ -45,18 +49,48 @@ it("renders players without error", async () => {
       </Provider>
     </MockedProvider>
   );
-  // expect(await screen.findByText("Whatever")).toBeInTheDocument();
 
   // Assertion 1: Check if the player components are rendered
   const playerComponents = screen.getAllByTestId("player-component");
+  // Iterate through the player components and check for the presence of specific attributes
+  playerComponents.forEach((playerComponent, index) => {
+    const player = players[index];
+    const playerName = `${player.firstname} ${player.lastname}`;
+    const pictureUrl = player.picture.url;
+
+    // Check for the player name
+    expect(playerComponent).toHaveTextContent(playerName);
+
+    // Check for the player picture
+    const imageElement = playerComponent.querySelector(
+      `img[src="${pictureUrl}"]`
+    );
+    expect(imageElement).toBeInTheDocument();
+
+    // Check for other attributes
+    expect(playerComponent).toHaveTextContent(`Sex: ${player.sex}`);
+    expect(playerComponent).toHaveTextContent(`Rank: ${player.stats.rank}`);
+    expect(playerComponent).toHaveTextContent(`Age: ${player.stats.age}`);
+    expect(playerComponent).toHaveTextContent(
+      `Weight: ${(player.stats.weight / 1000).toFixed(2)} kg`
+    );
+    expect(playerComponent).toHaveTextContent(
+      `Height: ${(player.stats.height / 100).toFixed(2)} m`
+    );
+    expect(playerComponent).toHaveTextContent(`Points: ${player.stats.points}`);
+    expect(playerComponent).toHaveTextContent(
+      `Total play time: ${calculateTotalPlayTime(player, matches)}`
+    );
+    expect(playerComponent).toHaveTextContent(
+      `Wins: ${getWinsAndLossesByPlayer(player, matches).wins}`
+    );
+    expect(playerComponent).toHaveTextContent(
+      `Losses: ${getWinsAndLossesByPlayer(player, matches).losses}`
+    );
+  });
+
+  // Finally, check if the correct number of player components are rendered
   expect(playerComponents).toHaveLength(players.length);
-
-  // Assertion 2: Check if player details are visible
-  const firstPlayer = players[0];
-  const playerName = `${firstPlayer.firstname} ${firstPlayer.lastname}`;
-  expect(screen.getByText(playerName)).toBeInTheDocument();
-
-  // You can add similar assertions for other player details.
 
   // Assertion 3: Check if the modal is opened when clicking on a player
   fireEvent.click(playerComponents[0]);
