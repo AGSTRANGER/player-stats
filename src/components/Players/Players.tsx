0caa632ad6d034c355656cab9f55ea964b134daf
AGSTRANGER <hannibaletunisien@gmail.com>
2023-11-01 07:43:43 +0100

    Improved players test

--- src/components/Players/Players.tsx
+++ src/components/Players/Players.tsx
@@ -32,6 +32,7 @@ export default function Players() {
 
   return (
     <div className="flex flex-wrap justify-center items-center">
+      Whatever
         {
       players?.map((player:Player)=>(<PlayerComponent key={player.id} player={player} onClick={() => handlePlayerClick(player)} />))
     }
--- src/tests/components/PlayersToTest.test.js
+++ src/tests/components/PlayersToTest.test.js
@@ -1,21 +1,9 @@
 import "@testing-library/jest-dom";
-import configureStore from "redux-mock-store";
-import { Provider } from "react-redux";
 import { render, screen } from "@testing-library/react";
 import { MockedProvider } from "@apollo/client/testing";
-import { players, matches } from "../sampleData";
-import Players from "../../components/Players/Players";
+import { players } from "../sampleData";
+import { Players } from "../../components/Players/PlayersToTest";
 import { LOAD_PLAYERS } from "../../GraphQL/PlayerQueries";
-
-const mockStore = configureStore([]);
-const store = mockStore({
-  matches: {
-    matches: matches,
-  },
-  players: {
-    players: players,
-  },
-});
 
 const mocks = [
   {
@@ -31,9 +19,7 @@ const mocks = [
 it("renders players without error", async () => {
   render(
     <MockedProvider mocks={mocks} addTypename={false}>
-      <Provider store={store}>
-        <Players />
-      </Provider>
+      <Players />
     </MockedProvider>
   );
   expect(await screen.findByText("Whatever")).toBeInTheDocument();
