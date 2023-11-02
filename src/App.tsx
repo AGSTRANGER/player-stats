af09565dd27a26b887f792eec74bf4308f59ece3
AGSTRANGER <hannibaletunisien@gmail.com>
2023-11-02 16:41:03 +0100

    Moved graphql setup to a separatae file

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,10 @@
 import "./App.css";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  HttpLink,
-  from,
-} from "@apollo/client";
-import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
-
-import { onError } from "@apollo/client/link/error";
+import React from "react";
+import { ApolloProvider } from "@apollo/client";
 import Players from "./components/Players/Players";
-
 import store from "./redux/store";
 import { Provider } from "react-redux";
-
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) {
-    graphQLErrors.map(({ message }) => {
-      alert(`Graphql error ${message}`);
-    });
-  }
-});
-
-const link = from([
-  errorLink,
-  new HttpLink({
-    uri: "https://kf9p4bkih6.execute-api.eu-west-1.amazonaws.com/dev/",
-  }),
-]);
-// #TODO
-// Set this only in dev env
-if (true) {
-  // Adds messages only in a dev environment
-  loadDevMessages();
-  loadErrorMessages();
-}
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: link,
-});
+import { client } from "./GraphQL/graphql";
 
 function App() {
   return (
---
+++ src/GraphQL/graphql.ts
@@ -0,0 +1,30 @@
+import { ApolloClient, InMemoryCache, HttpLink, from } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
+
+const errorLink = onError(({ graphQLErrors }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({
+    uri: "https://kf9p4bkih6.execute-api.eu-west-1.amazonaws.com/dev/",
+  }),
+]);
+
+// Set this only in dev env
+if (process.env.NODE_ENV === "development") {
+  // Adds messages only in a dev environment
+  loadDevMessages();
+  loadErrorMessages();
+}
+
+export const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link,
+});
